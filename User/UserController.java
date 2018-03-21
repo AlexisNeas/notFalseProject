17043da17d79776b2df3850946b181948aa9cd91
ANEAS001 <aneas001@vplincsci045.ad.csbsju.edu>
2018-03-20 20:18:54 -0500

    Complete javadoc for UserController and UserInteraction

--- User/UserController.java
+++ User/UserController.java
@@ -4,10 +4,10 @@ import DatabaseController.*;
 import University.*;
 
 /**
- * Class that controls the user interactions, and interacts with the database
+ * Class that interacts with the contructed Database Controller and account controller. Interacts with the classes based on User Interactions
  * 
  * @author Alexis Neas
- * @version 3
+ * @version 3/20/18
  */
 
 public class UserController {
@@ -16,6 +16,9 @@ public class UserController {
  private University university;
  private AccountController accountController;
  
+ /**
+  * User Controller constructor
+  */
  public UserController()
  {
    this.dbController = new DBController("notfal", "csci230");
@@ -47,8 +50,13 @@ public class UserController {
    * @param upSocialScale Upper limit for Social Scale.
    * @param lowQualityOfLifeScale Lower limit for Quality Of Life Scale.
    * @param upQualityOfLifeScale Upper limit for Quality Of Life Scale.
+   * @oaram emphases1 one of the emphases of the university
+   * @oaram emphases2 one of the emphases of the university
+   * @oaram emphases3 one of the emphases of the university
+   * @oaram emphases4 one of the emphases of the university
+   * @oaram emphases5 one of the emphases of the university
    * 
-   * @return the university objects closest to the search
+   * @return the university objects closest to the search in an ArrayList
    */
   public ArrayList <University> searchSchool(String schoolName, String stateName, String location, String control,
                            int lowNumberOfStudents, int upNumberOfStudents,  
@@ -74,7 +82,7 @@ public class UserController {
   /**
    * Removes a school from a user's list of saved schools
    * 
-   * @param school the name of university to be removed
+   * @param university the name of university to be removed
    * @param user the user that the university is being removed from
    * 
    * @return returns a boolean to indicate the university has been removed
@@ -88,7 +96,7 @@ public class UserController {
   /**
    * Gets the information for the selected school
    * 
-   * @param school the name of the school that is to be removed
+   * @param school the name of the school to get the information about
    * 
    * @return returns a University containing all of the information
    */
@@ -111,30 +119,8 @@ public class UserController {
   /**
    * Find similar schools to the one selected
    * 
-   * @param schoolName Name of the school to search for.
-   * @param stateName Name of the state for schools.
-   * @param location Location area of a school.
-   * @param control Private State or City control.
-   * @param lowNumberOfStudents Lower limit for student population.
-   * @param upNumberOfStudents Upper limit for student population.
-   * @param lowPercentFemale Lower limit for Percent Female.
-   * @param upPercentFemale Upper limit for Percent Female.
-   * @param lowSATVerbal Lower limit for SATVerbal.
-   * @param upSATVerbal Upper limit for SATVerbal.
-   * @param lowSATMath Lower limit for SATMath.
-   * @param upSATMath Upper limit for SATMath.
-   * @param lowExpenses Lower limit for Expenses.
-   * @param upExpenses Upper limit for Expenses.
-   * @param lowPercentEnrolled Lower limit for Percent Enrolled.
-   * @param upPercentEnrolled Upper limit for Percent Enrolled.
-   * @param lowAcademicsScale Lower limit for Academics Scale.
-   * @param upAcademicsScale Upper limit for Academics Scale.
-   * @param lowSocialScale Lower limit for Social Scale.
-   * @param upSocialScale Upper limit for Social Scale.
-   * @param lowQualityOfLifeScale Lower limit for Quality Of Life Scale.
-   * @param upQualityOfLifeScale Upper limit for Quality Of Life Scale.
-   * 
-   * @return list of similar schools
+   * @param university the university to be compared to
+   * @return list of 5 universities most similar to university
    */
   public ArrayList<University> findSimilarSchools(University university)
   {
@@ -144,21 +130,25 @@ public class UserController {
   /**
    * Displays the results of a search
    * 
-   * @param schools list of schools that should be displayed
+   * @param universities list of schools that should be displayed
    */
   public void displayResults(ArrayList<University> universities)
   {
     if (universities == null)
       System.out.println("No universities to display");
     else
-    for( University u:universities)
     {
-      System.out.println("University: "+ u.getSchoolName()); 
+      for( University u:universities)
+      {
+        System.out.println("University: "+ u.getSchoolName()); 
+      }
     }
   }
   
   /**
    * Gets a list of user's saved universities
+   * 
+   * @param username the username to get the saved universities from
    * 
    * @return the list of schools that have been saved by user
    */
@@ -170,27 +160,29 @@ public class UserController {
   /**
    * Edits the user's profile and saves changes
    * 
-   * @param first the user's first name
-   * @param last the user's last name
+   * @param username the user's username
    * @param password the user's password
-   * 
-   * @return returns true if the profile has been successfully saved
+   * @param firstName the user's first name
+   * @param lastName the user's last name
+   * @param type the type of account
+   * @param status the status of the account
    */
   public void editProfile(String username, String password, String firstName, String lastName, char type, char status)
   {
    dbController.setUserInfo(  username,  password,  firstName,  lastName,  type,  status);
   }
+  
   /**
+   * Allows the user to view their profile
    * 
+   * @param username the user's username
    * 
+   * @return an account object with the users information
    * 
-   * @param username
-   * @return account an account object with the users information.
    */
   public Account viewProfile(String username)
   {
-    Account acct = dbController.getUserInfo(username);
-    return acct;
+    return dbController.getUserInfo(username);
   }
   
 }
