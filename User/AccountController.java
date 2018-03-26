0b9998dd59a203ed04e27a8bf3c17b8dabda9788
ZVETTER001 <zvetter001@vplincsci079.ad.csbsju.edu>
2018-03-26 18:54:40 -0500

    deleted commented methods

--- User/AccountController.java
+++ User/AccountController.java
@@ -66,43 +66,4 @@ public class AccountController {
     account.logOff();
     
   }
-  
-  /**
-   * This allows the user to recover their password if they forgot it.
-   * They enter their username and are able to retrieve.
-   * @return String of the user's password
-   */
-//  public Account getPassword(){
-//  //  Scanner in = new Scanner(System.in);
-//   // System.out.println("Enter your username: ");
-//    //String user = in.nextLine();
-//    Account userInfo = dbController.getUserInfo(user);
-//    return userInfo;
-//  }
-  
-
-  
-  /**
-   * Checks if a specific user has an active accountor not
-   * displays an error message if their account is not active
-//   */
-//
-//  public boolean getActive() {
-//   char ch = account.getStatus();
-//   if(ch == 'Y'){
-//     return true;
-//   }
-//   //search through list of info for active/nonactive char
-//   //if statement confirming whether the user has an active or inactive account
-//   //displays an error if the account is inactive
-//  else {
-//    return false;
-//   //Account doesn't exist
-//     
-//  }
-//  }
-  
-
-  
-  
 }
