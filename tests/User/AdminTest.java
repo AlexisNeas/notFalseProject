ba8a2bf63ebad35f36d669b3680530481b7c5be1
JHANSEN001 <jhansen001@vplincsci083.ad.csbsju.edu>
2018-04-03 13:39:26 -0500

    Changes to Account tests

---
+++ tests/User/AdminTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package User;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author jhansen001
+ *
+ */
+public class AdminTest {
+
+	Admin admin;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		this.admin = new Admin("jadmin", "admin", "jadmin", "admin", "a", "Y");
+	}
+
+	@Test
+	public void testAdminConstructor() {
+		
+		Admin result = this.admin;
+		Admin expResult = new Admin("jadmin", "admin", "jadmin", "admin", "a", "Y")
+		assert.assertEquals("Admin account exists.",expResult, result);
+	}
+
+}
