102773dec391121ec17a7a3f1b4e9f0120a7fe63
ZVETTER001 <zvetter001@vplincsci031.ad.csbsju.edu>
2018-04-08 18:54:46 -0500

    minor changes

--- tests/University/UniversityControllerTest.java
+++ tests/University/UniversityControllerTest.java
@@ -6,16 +6,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class UniversityControllerTest {
-
+	private University testSchool;
  
+@Before
  public void setUp() throws Exception {
+	  testSchool = new University("Vetters School", "NORTH DAKOTA", "BISMARCK",
+				"STATE", 5, 1.0, 1.0, 1.0, 1.0, 1.0, 1, 1.0, 1.0, 1, 1, 1, "",
+				"", "", "", "");
  }
 
 
- 
+ @Test
  public void testMakeUniversity() 
  {
-   
+	 University newSchool = new University("Vetters School", "NORTH DAKOTA", "BISMARCK",
+				"STATE", 5, 1.0, 1.0, 1.0, 1.0, 1.0, 1, 1.0, 1.0, 1, 1, 1, "",
+				"", "", "", "");
+	 assertTrue("The schools do not match", newSchool.equals(testSchool));
  }
 
 }
