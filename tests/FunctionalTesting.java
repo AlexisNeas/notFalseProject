9e9ee2d0b6637a43d5b46b0ae030455a7b5bd4bf
ANEAS001 <aneas001@vplincsci030.ad.csbsju.edu>
2018-04-09 22:11:01 -0500

    FunctionalTesting 1-7

--- tests/FunctionalTesting.java
+++ tests/FunctionalTesting.java
@@ -7,18 +7,66 @@ import University.*;
 import User.*;
 
 public class FunctionalTesting {
-
+	private UserInteraction userInteractions;
+	private AdminInteractions adminInteractions;
+	private AccountInteractions accountInteractions;
+	
 	@Before
 	public void setUp() throws Exception {
-		UserInteraction userInteractions = new UserInteraction();
-		AdminInteractions adminInteractions = new AdminInteractions();
-		AccountInteractions accountInteractions = new AccountInteractions();
+		userInteractions = new UserInteraction();
+		adminInteractions = new AdminInteractions();
+		accountInteractions = new AccountInteractions();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLogOnU1U3() {
+		Account account = accountInteractions.logOn("juser", "user");
+		boolean loggedOn = account.isLoggedOn();
+		boolean expected = true;
+		assertTrue("The account should be logged on", loggedOn==expected);
 	}
+	
+	@Test
+	public void testLogOnU1U2() {
+		Account account = accountInteractions.logOn("nadmin", "admin");
+		boolean loggedOn = account.isLoggedOn();
+		boolean expected = true;
+		assertTrue("The account should be logged on", loggedOn==expected);
+	}
+	
+	@Test
+	public void testLogOnAlt1InvalidUser() {
+		Account account = accountInteractions.logOn("THISISNOTAUSERNAME", "password");
+		assertTrue("The account should not be logged on", account==null);
+	}
+	
+	@Test
+	public void testLogOnAlt2InvalidPassword() {
+		Account account = accountInteractions.logOn("juser", "WRONGPASSWORD");
+		boolean loggedOn = account.isLoggedOn();
+		boolean expected = false;
+		assertTrue("The account should not be logged on", loggedOn==expected);
+	}
+	
+	@Test
+	public void testLogOff() {
+		Account account = accountInteractions.logOn("juser", "user");
+		boolean loggedOn = account.isLoggedOn();
+		account.logOff();
+		boolean loggedOff = account.isLoggedOn();
+		assertTrue("Account is not logged off", loggedOn != loggedOff);
+	}
+	
+	@Test
+	public void testViewUniversities() {
+		
+	}
+	
+	@Test
+	public void testEditSchool() {
+		
+	}
+	
 	
 	
 	
